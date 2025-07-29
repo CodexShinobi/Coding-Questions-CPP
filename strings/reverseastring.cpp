49a67f4bcecbbea88691a82092723505e554f84d
Navneet Prakash <142720492+CodexShinobi@users.noreply.github.com>
2025-07-29 16:02:57 +0530

    Add files via upload

---
+++ strings/reverseastring.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+
+ int main(){
+    string str="navneet prakash.";
+    reverse(str.begin(),str.end());
+    cout<<str<<endl;
+    return 0;
+ }
